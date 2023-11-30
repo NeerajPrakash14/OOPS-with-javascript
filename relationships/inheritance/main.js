9d12a274626eb12f88135c70bb2ce68e19576b6b
Neeraj Prakash <neerajprakashreddy@gmail.com>
2023-11-30 11:15:16 +0000

    inheritance

---
+++ relationships/inheritance/main.js
@@ -0,0 +1,28 @@
+// Define mixins with specific functionalities
+const Printable = {
+    print() {
+      console.log(this.content);
+    },
+  };
+  
+  const Serializable = {
+    serialize() {
+      return JSON.stringify(this.content);
+    },
+  };
+  
+  // Create a class that uses mixins
+  class MyCombinedClass {
+    constructor(content) {
+      this.content = content;
+    }
+  }
+  
+  // Applying mixins to the class
+  Object.assign(MyCombinedClass.prototype, Printable, Serializable);
+  
+  // Usage
+  const myObj = new MyCombinedClass('Sample Content');
+  myObj.print(); // Output: Sample Content
+  console.log(myObj.serialize()); // Output: "Sample Content"
+  
